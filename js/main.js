fae1295ca83c5875df01cc69366af88c8ae2aaad
Richard Lasso <lasso_1995@hotmail.com>
2020-08-01 21:05:18 -0500

    Finalized mobile header

---
+++ js/main.js
@@ -0,0 +1,37 @@
+/* HAMBURGUER MENU */
+
+// Variables
+const bars = document.getElementById('bars'),
+    close = document.getElementById('close'),
+    activeMenu = document.querySelector('.active-menu'),
+    menuList = document.querySelector('.menu'),
+    body = document.querySelector('body');
+
+    console.log(menuList)
+
+// Event Listeners
+
+bars.addEventListener('click', (e) => {
+    e.preventDefault();
+    close.style.display = 'inline-block';
+    bars.style.transform = 'rotate(360deg)';
+    if(menuList.classList.contains('active-menu')) {
+        menuList.classList.remove('active-menu');
+        body.style.overflow = 'visible';
+    }   else {
+        menuList.classList.add('active-menu');
+        body.style.overflow = 'hidden';
+    }
+});
+
+close.addEventListener('click', (e) => {
+    e.preventDefault();
+    close.style.display = 'none';
+    if(menuList.classList.contains('active-menu')) {
+        menuList.classList.remove('active-menu');
+        body.style.overflow = 'visible';
+    }   else {
+        menuList.classList.add('active-menu');
+        body.style.overflow = 'hidden';
+    }
+});
