b58353d5d298e00494e1edd3903d7884c9fdfbbb
Richard Lasso <lasso_1995@hotmail.com>
2020-08-02 21:13:07 -0500

    finalized json files integration

--- js/main.js
+++ js/main.js
@@ -34,31 +34,6 @@ close.addEventListener('click', (e) => {
     }
 });
 
-
-// bxslider
-
-$('.bxslider').bxSlider({
-    auto: true,
-    stopAutoOnClick: true,
-    pager: true,
-    minSlides: 2,
-    maxSlides: 4,
-    slideMargin: 16,
-    slideWidth: 220
-    
-});
-
-$('.bxslider-publications').bxSlider({
-    auto: true,
-    stopAutoOnClick: true,
-    pager: true,
-    minSlides: 1,
-    maxSlides: 2,
-    slideMargin: 16,
-    slideWidth: 600
-    
-});
-
 // menu slider
 
 window.onscroll = function () {
@@ -74,3 +49,50 @@ window.onscroll = function () {
         header.style.transition = 'all ease .5s';
     }
 };
+
+
+// json files
+
+fetch('https://json.extendsclass.com/bin/44f9f614419b')
+    .then(response => response.json())
+    .then(json => {
+        var radr = JSON.stringify(json)
+        console.log(json)
+        document.getElementById('main-title').innerHTML = json.mainTitle;
+        document.getElementById('carousel-item1-image').src = json.carouselItem1Image;
+        document.getElementById('carousel-item1-title').innerHTML = json.carouselItem1Title;
+        document.getElementById('carousel-item1-body').innerHTML = json.carouselItem1Body;
+        document.getElementById('carousel-item1-button').innerHTML = json.carouselItem1Button;
+        document.getElementById('carousel-item2-image').src = json.carouselItem2Image;
+        document.getElementById('carousel-item2-title').innerHTML = json.carouselItem2Title;
+        document.getElementById('carousel-item2-body').innerHTML = json.carouselItem2Body;
+        document.getElementById('carousel-item2-button').innerHTML = json.carouselItem2Button;
+
+        $('.bxslider-publications').bxSlider({
+            auto: true,
+            stopAutoOnClick: true,
+            pager: true,
+            minSlides: 1,
+            maxSlides: 2,
+            slideMargin: 16,
+            slideWidth: 600
+            
+        });
+    });
+
+// bxslider
+
+$('.bxslider').bxSlider({
+    auto: true,
+    stopAutoOnClick: true,
+    pager: true,
+    minSlides: 2,
+    maxSlides: 4,
+    slideMargin: 16,
+    slideWidth: 220
+    
+});
+
+
+
+
